bcdf24c804b78e9ba666d0e686905fc91d3000df
Victor <victor@github.com>
2019-10-15 00:56:34 +0530

    Initial COmmit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const fs = require('fs');
+
+hbs.registerPartials(__dirname + '/Views/Partials');
+app.set('view engine', 'hbs');
+/* app.use(express.static(__dirname + '/public')); */
+
+app.use((req,res,next) => {
+    var now = new Date().toDateString();
+    var log = `${now}: ${req.method}: ${req.url}`;
+
+   // console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Error!');
+        }
+    });
+    next();
+});
+
+/* app.use((req,res,next) => {
+    res.render('maintenance.hbs');
+}); */
+
+app.use(express.static(__dirname + '/public')); //It's shifted over here coz app.use works in a sequential
+                                                //Order. So, help.html wouldn't have been redirected in the maintenance page.
+
+
+/* app.get('/', (req,res) => {
+    res.send('Hello World!');
+});
+ */
+
+app.get('/',(req,res) => {
+    res.render('home.hbs',{
+        title: 'This is my server!',
+        CurrentDate: new Date().getDate()
+    });
+});
+ 
+app.get('/about', (req,res) => {
+    res.render('about.hbs',{
+        name: 'Indranil Palit',
+        hobbies: 'Playing Football',
+        DOB: '11/02/1999'
+    }); 
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Listening in Port 3000');
+});
